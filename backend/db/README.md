df5231ee09c839f67e0fb9ed7c88eb82c895e149
Marko Heikkilä <heikkila_marko@hotmail.com>
2021-05-02 22:16:42 +0300

    Database migrations.

---
+++ backend/db/README.md
@@ -0,0 +1,11 @@
+# How to run database migrations
+
+```bash
+docker-compose run --rm migrate
+```
+
+or
+
+```bash
+docker compose run --rm migrate
+```
--- backend/db/migrate.sh
+++ backend/db/migrate.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-export $(grep -v '^#' ../../env/migrate.env | xargs)
-
-docker run \
-  --rm \
-  --mount type=bind,src=$(pwd)/migrations,dst=/migrations \
-  migrate/migrate -path /migrations \
-  -database $POSTGRES_CONNECTIONSTRING \
-  up
+migrate -path /migrations -database $POSTGRES_CONNECTIONSTRING up
