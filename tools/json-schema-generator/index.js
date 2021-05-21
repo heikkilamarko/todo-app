5f0ca67e1f1725bef5da158ecaddba1f14c3ac54
Marko Heikkilä <heikkila_marko@hotmail.com>
2021-05-21 23:21:40 +0300

    Improve json schema generation.

---
+++ tools/json-schema-generator/index.js
@@ -0,0 +1,56 @@
+import { readFile, writeFile } from "fs/promises";
+import parser from "@asyncapi/parser";
+import chalk from "chalk";
+import YAML from "yaml";
+
+const configFile = process.argv.slice(2)[0];
+
+if (!configFile) {
+  console.error(
+    "error: You must pass a config yaml file as the only argument."
+  );
+  process.exit(1);
+}
+
+let config = await readFile(configFile, "utf8");
+
+config = YAML.parse(config);
+
+for (const service of config.services) {
+  logDivider();
+  await generate(service);
+}
+
+logDivider();
+
+async function generate({ name, apiFile, outputDir }) {
+  console.log(chalk.yellow("service"));
+  console.log(chalk.cyan(`    ${name}`));
+  console.log(chalk.yellow("input"));
+  console.log(chalk.cyan(`    ${apiFile}`));
+  console.log(chalk.yellow("output"));
+
+  const apiContent = await readFile(apiFile, "utf8");
+
+  let {
+    _json: { channels },
+  } = await parser.parse(apiContent);
+
+  const messages = Object.entries(channels)
+    .filter(([_, c]) => c.publish)
+    .map(([name, c]) => ({
+      file: `${outputDir}/${name}.json`,
+      schema: JSON.stringify(c.publish.message.payload, null, 2),
+    }));
+
+  for (const m of messages) {
+    await writeFile(m.file, m.schema);
+    console.log(chalk.green(`    ${m.file}`));
+  }
+}
+
+function logDivider() {
+  console.log();
+  console.log(chalk.yellow(`----------------------------------------`));
+  console.log();
+}
