402f84efb24134a5d22304442802acf4b180558f
Marko Heikkilä <heikkila_marko@hotmail.com>
2021-06-07 15:36:59 +0300

    Improve monitoring configuration.

--- monitor/minio/migrate/migrate.sh
+++ monitor/minio/migrate/migrate.sh
@@ -2,5 +2,5 @@
 
 /bin/sh -c
 /usr/bin/mc config host rm local;
-/usr/bin/mc config host add --quiet --api s3v4 local http://minio:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY;
+/usr/bin/mc config host add --quiet --api s3v4 local $MINIO_URL $MINIO_ACCESS_KEY $MINIO_SECRET_KEY;
 /usr/bin/mc mb --ignore-existing --quiet local/loki/;
