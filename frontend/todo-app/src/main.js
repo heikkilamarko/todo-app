bb4215f89ad7bec7f6e946683b83ce8020bf3921
Marko Heikkilä <heikkila_marko@hotmail.com>
2021-06-11 17:52:02 +0300

    Improve bootstrap error handling.

--- frontend/todo-app/src/main.js
+++ frontend/todo-app/src/main.js
@@ -14,7 +14,15 @@ async function bootstrap() {
       });
   } catch (error) {
     console.log(error);
+    renderError(error);
   }
+}
+
+/**
+ * @param {Error} error
+ */
+function renderError(error) {
+  document.body.innerHTML = `<main class="px-4 py-5 overflow-auto d-flex flex-column align-items-center vh-100 bg-danger text-white"><h1 class="display-1 fw-lighter">Close, but No Cigar</h1><p class="pt-2">${error}</p></main>`;
 }
 
 bootstrap();
