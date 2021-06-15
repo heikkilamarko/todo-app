0301cbe01bc053fcff837b89f2603245de41ac8d
Marko Heikkilä <heikkila_marko@hotmail.com>
2021-06-15 12:46:21 +0300

    Take bootstrap offcanvas into use.

--- frontend/todo-app/src/components/TodoForm.svelte
+++ frontend/todo-app/src/components/TodoForm.svelte
@@ -2,31 +2,69 @@
   import { name, description, canCreate, create } from "../stores/todoStore";
 </script>
 
-<form spellcheck="false" autocomplete="off" on:submit|preventDefault={create}>
-  <div class="mb-3">
-    <label for="name" class="form-label"
-      >Name <span class="text-danger">*</span></label
+<button
+  class="btn btn-primary rounded-pill px-3"
+  type="button"
+  data-bs-toggle="offcanvas"
+  data-bs-target="#offcanvas-right"
+  aria-controls="offcanvas-right"
+>
+  <i class="bi bi-plus-lg" />
+  New Todo
+</button>
+
+<div
+  id="offcanvas-right"
+  class="offcanvas offcanvas-end"
+  tabindex="-1"
+  data-bs-scroll="true"
+  data-bs-backdrop="false"
+  aria-labelledby="offcanvas-right-label"
+>
+  <div class="offcanvas-header">
+    <h5 id="offcanvas-right-label">New Todo</h5>
+    <button
+      type="button"
+      class="btn-close text-reset"
+      data-bs-dismiss="offcanvas"
+      aria-label="Close"
+    />
+  </div>
+  <div class="offcanvas-body">
+    <form
+      spellcheck="false"
+      autocomplete="off"
+      on:submit|preventDefault={create}
     >
-    <input
-      type="text"
-      class="form-control"
-      id="name"
-      placeholder="Name..."
-      bind:value={$name}
-    />
+      <div class="mb-3">
+        <label for="name" class="form-label"
+          >Name <span class="text-danger">*</span></label
+        >
+        <input
+          type="text"
+          class="form-control"
+          id="name"
+          placeholder="Name..."
+          bind:value={$name}
+        />
+      </div>
+      <div class="mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea
+          class="form-control"
+          id="description"
+          rows="3"
+          bind:value={$description}
+        />
+      </div>
+      <button
+        type="submit"
+        class="btn btn-primary rounded-pill px-3"
+        disabled={!$canCreate}
+      >
+        <i class="bi bi-plus-lg" />
+        Create
+      </button>
+    </form>
   </div>
-  <div class="mb-3">
-    <label for="description" class="form-label">Description</label>
-    <textarea
-      class="form-control"
-      id="description"
-      rows="3"
-      bind:value={$description}
-    />
-  </div>
-  <button
-    type="submit"
-    class="btn btn-primary rounded-pill px-3"
-    disabled={!$canCreate}><i class="bi bi-plus-lg" /> Create</button
-  >
-</form>
+</div>
--- frontend/todo-app/src/main.js
+++ frontend/todo-app/src/main.js
@@ -1,11 +1,12 @@
 import "bootstrap/js/dist/dropdown";
+import "bootstrap/js/dist/offcanvas";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./main.scss";
 import { load as loadConfig } from "./shared/config";
 import { init as initAuth } from "./shared/auth";
 import App from "./components/App.svelte";
 
-async function bootstrap() {
+async function run() {
   try {
     await loadConfig();
     const isAuthenticated = await initAuth();
@@ -26,4 +27,4 @@ function renderError(error) {
   document.body.innerHTML = `<main class="px-4 py-5 overflow-auto d-flex flex-column align-items-center vh-100 bg-danger text-white"><h1 class="display-1 fw-lighter">Close, but No Cigar</h1><p class="pt-2">${error}</p></main>`;
 }
 
-bootstrap();
+run();
