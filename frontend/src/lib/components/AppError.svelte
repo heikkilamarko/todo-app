9e7d80078d14de586c32e688a798802956b1d0fa
Marko Heikkilä <heikkila_marko@hotmail.com>
2021-10-18 01:15:31 +0300

    Code cleanup.

--- frontend/src/lib/components/AppError.svelte
+++ frontend/src/lib/components/AppError.svelte
@@ -6,5 +6,5 @@
   class="px-4 py-5 overflow-auto d-flex flex-column align-items-center vh-100 bg-danger text-white"
 >
   <h1 class="display-1 fw-lighter">Close, but No Cigar</h1>
-  <p class="pt-2">${error}</p>
+  <p class="pt-2">{error}</p>
 </main>
