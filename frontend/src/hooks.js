242c71b76382ed62004d21cfdfcf33aa052b6f52
Marko Heikkilä <heikkila_marko@hotmail.com>
2022-01-20 15:11:58 +0200

    Update deps.

--- frontend/src/hooks.js
+++ frontend/src/hooks.js
@@ -1,6 +1,6 @@
 /** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ request, resolve }) {
-  return await resolve(request, {
+export async function handle({ event, resolve }) {
+  return await resolve(event, {
     ssr: false,
   });
 }
