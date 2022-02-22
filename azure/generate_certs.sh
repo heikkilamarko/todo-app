43e8a4b4af5eb95a819da31a938974954d9e60ee
Marko Heikkilä <heikkila_marko@hotmail.com>
2022-02-22 13:51:35 +0200

    Cleanup.

--- azure/generate_certs.sh
+++ azure/generate_certs.sh
@@ -5,7 +5,6 @@ docker context use todo-app
 docker run -it --rm --name certbot \
   -v "/etc/letsencrypt:/etc/letsencrypt" \
   -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-  -p 80:80 \
   certbot/certbot certonly \
   --manual \
   --preferred-challenges=dns \
