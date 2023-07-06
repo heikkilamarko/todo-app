f5592eb92125392f8268fd969215c4871bcdc12d
Marko Heikkilä <heikkila_marko@hotmail.com>
2023-07-06 17:30:28 +0300

    Update minio migrate script.

--- monitor/minio/migrate/migrate.sh
+++ monitor/minio/migrate/migrate.sh
@@ -7,10 +7,12 @@ curl -s $MINIO_URL > /dev/null
 set +o history
 
 mc alias set minio $MINIO_URL $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
-mc mb --ignore-existing minio/loki
 
-mc admin user add minio $MINIO_CONSOLE_USER $MINIO_CONSOLE_PASSWORD
-mc admin policy create minio consoleAdmin consoleAdmin.json
-mc admin policy attach minio consoleAdmin --user $MINIO_CONSOLE_USER
+if ! mc admin user info minio "$MINIO_CONSOLE_USER" | grep -q consoleAdmin; then
+    mc mb --ignore-existing minio/loki
+    mc admin user add minio $MINIO_CONSOLE_USER $MINIO_CONSOLE_PASSWORD
+    mc admin policy create minio consoleAdmin consoleAdmin.json
+    mc admin policy attach minio consoleAdmin --user $MINIO_CONSOLE_USER
+fi
 
 set -o history
