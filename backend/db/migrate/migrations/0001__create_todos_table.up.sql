fa48ee50e9ebf8192de78f916903ad3df3985c20
Marko Heikkilä <heikkila_marko@hotmail.com>
2025-02-22 17:48:16 +0200

    SQL cleanup.

--- backend/db/migrate/migrations/0001__create_todos_table.up.sql
+++ backend/db/migrate/migrations/0001__create_todos_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE todos (
-    id SERIAL PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT,
-    created_at timestamptz NOT NULL,
-    updated_at timestamptz NOT NULL
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
 );
--- backend/db/migrate/migrations/0003__create_auth_tables.up.sql
+++ backend/db/migrate/migrations/0003__create_auth_tables.up.sql
@@ -1,15 +1,15 @@
 CREATE TABLE roles (
-    id SERIAL PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE permissions (
-    id SERIAL PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE role_permissions (
-    role_id INTEGER REFERENCES roles(id),
-    permission_id INTEGER REFERENCES permissions(id),
+    role_id BIGINT REFERENCES roles(id),
+    permission_id BIGINT REFERENCES permissions(id),
     PRIMARY KEY (role_id, permission_id)
 );
