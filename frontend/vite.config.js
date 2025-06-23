2acef6dc70d4dfcc57cb2db0e6bf68340df8b85d
Marko Heikkilä <heikkila_marko@hotmail.com>
2025-06-23 22:16:09 +0300

    Frontend cleanup.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), devtoolsJson()],
 	server: {
 		proxy: {
 			'/api': {
